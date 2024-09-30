26515f9663764587727741fe159ebe1693991e95
Douglas Peterson <douglaspeterson6@gmail.com>
2024-09-30 07:45:23 -0500

    added factor function to follow order of operations

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -9,15 +9,15 @@ use nom::{
 };
 
 
-#[derive(Debug)]
-pub enum Expr {
+#[derive(Debug, PartialEq)]
+pub enum Expression {
     Number(i64),
     String(String),
     Variable(String),
-    BinaryOp(Box<Expr>, BinaryOperator, Box<Expr>),
+    BinaryOp(Box<Expression>, BinaryOperator, Box<Expression>),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum BinaryOperator {
     Add,
     Subtract, 
@@ -25,12 +25,12 @@ pub enum BinaryOperator {
     Divide
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Stmt {
-    Assignment(String, Expr)
+    Assignment(String, Expression)
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Program {
     pub statements: Vec<Stmt>,
 }
@@ -50,17 +50,19 @@ fn main() {
     }
 }
 
-fn parse_number(input: &str) -> IResult<&str, Expr> {
+fn parse_number(input: &str) -> IResult<&str, Expression> {
     map(
         preceded(multispace0, digit1),
-         |n: &str| Expr::Number(n.parse().unwrap()))(input)
+         |n: &str| Expression::Number(n.parse().unwrap()))(input)
 }
 
-fn parse_identifier(input: &str) -> IResult<&str, Expr> {
+// Parse Variables (identifiers)
+fn parse_identifier(input: &str) -> IResult<&str, Expression> {
     map(
         preceded(multispace0, alpha1),
-         |var: &str| Expr::Variable(var.to_string()))(input)
+         |var: &str| Expression::Variable(var.to_string()))(input)
 }
+
 
 fn parse_operator(input: &str) -> IResult<&str, BinaryOperator> {
     alt((
@@ -71,13 +73,39 @@ fn parse_operator(input: &str) -> IResult<&str, BinaryOperator> {
     ))(input)
 }
 
-fn parse_expression(input: &str) -> IResult<&str, Expr> {
-
+// parse for factors
+fn parse_factor(input: &str) -> IResult<&str, Expression> {
+    
     let (input, init) = alt((parse_number, parse_identifier))(input)?;
-    let (input, result) = many0(pair(parse_operator, alt((parse_number, parse_identifier))))(input)?;
+    let (input, result) = many0(pair(
+        alt((
+            map(preceded(multispace0, tag("*")), |_| BinaryOperator::Multiply),
+            map(preceded(multispace0, tag("/")), |_| BinaryOperator::Divide)
+        )),
+        alt((parse_number, parse_identifier))
+    ))(input)?;
+
+    let expr = result.into_iter().fold(init, |acc, (op, right)| {
+        Expression::BinaryOp(Box::new(acc), op, Box::new(right))
+    });
+
+    Ok((input, expr))
+}
+
+
+fn parse_expression(input: &str) -> IResult<&str, Expression> {
+
+    let (input, init) = parse_factor(input)?;
+    let (input, result) = many0(pair(
+        alt((
+            map(preceded(multispace0, tag("+")), |_| BinaryOperator::Add),
+            map(preceded(multispace0, tag("-")), |_| BinaryOperator::Subtract)
+        )),
+        parse_factor
+        ))(input)?;
 
     let expr = result. into_iter().fold(init, |acc, (op, right)| {
-        Expr::BinaryOp(Box::new(acc), op, Box::new(right))
+        Expression::BinaryOp(Box::new(acc), op, Box::new(right))
     });
 
     Ok((input, expr))
@@ -99,4 +127,48 @@ fn parse_program(input: &str) -> IResult<&str, Program> {
     Ok((input, Program { statements }))
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
+    // Tests order of operations and multiple lines/Assignments
+    #[test]
+    fn multi_line_program() {
+        let input = "x= 1 + 2 * 3\ny = 4 - 5";
+
+        let expected = Program {
+            statements: vec![
+                Stmt::Assignment(
+                    "x".to_string(),
+                    Expression::BinaryOp(
+                        Box::new(Expression::Number(1)),
+                        BinaryOperator::Add, 
+                        Box::new(Expression::BinaryOp(
+                            Box::new(Expression::Number(2)),
+                            BinaryOperator::Multiply,
+                            Box::new(Expression::Number(3))
+                        )),
+                    ),
+                ),
+                Stmt::Assignment(
+                    "y".to_string(),
+                        Expression::BinaryOp(
+                            Box::new(Expression::Number(4)),
+                            BinaryOperator::Subtract,
+                            Box::new(Expression::Number(5))
+                        ),
+                    ), 
+            ],
+        };
+
+        match parse_program(input) {
+            Ok((_, program)) => {
+                assert_eq!(program, expected);
+            }
+
+            Err(e) => panic!("Failed to parse program: {:?}", e),
+        }
+    }
+
+    
+}
